e5ef7cc818edefa6c530c3c08a91cfd8e534c7d0
Carly Sierra <carlylwomack@gmail.com>
2018-04-16 19:03:04 -0500

    added basic action and reducer

--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>, document.getElementById('root'));
 registerServiceWorker();
